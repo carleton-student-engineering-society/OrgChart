e11cf52b4fce4787db3601562760a432ba8355f6
finlaywashere <finman292004@protonmail.com>
2024-01-29 11:05:06 -0500

    Added models to admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,32 @@
 # from django.contrib import admin
 
-# Register your models here.
+from main.models import Person, Organization, Role, Term
+from django.contrib import admin
+
+
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Person, PersonAdmin)
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Organization, OrganizationAdmin)
+
+
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Role, RoleAdmin)
+
+
+class TermAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Term, TermAdmin)
