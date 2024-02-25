72e4207a43d677c86dccb972e3f8fa74ea821fca
Finlay Maroney <finman292004@protonmail.com>
2024-02-24 21:17:15 -0500

    Setup using mariadb

--- orgchart/settings.py
+++ orgchart/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'orgchart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'orgchart',
+        'USER': 'orgchart',
+        'PASS': 'orgchart',
+        'HOST': '127.0.0.1',
+        'CONN_MAX_AGE': 3600,
+        'PORT': '3306',
     }
 }
 
